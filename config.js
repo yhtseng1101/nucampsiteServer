4726dbb45f71aad856a88209338eed5946c1d3a8
yhtseng1101 <yhtseng1101@gmail.com>
2020-06-17 23:53:33 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite' 
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite' ,
+    'facebook': {
+        clientId: '934316950355127',
+        clientSecret: '4daf499d1d051ada84d6e7040912a3ff'
+    }
 }
